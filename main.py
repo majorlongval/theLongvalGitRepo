7df5df1bcd349b5dc23ae3369b9bd2d6a2e95fc1
JordanLongval <j.longval@robotiq.com>
2020-12-07 21:50:19 -0500

    Auto stash before rebase of "origin/Jordan_Day_19_v2"

--- main.py
+++ main.py
@@ -21,12 +21,14 @@ def turn_left():
 
 def clear_drawing():
     tim.clear()
+    tim.reset()
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
-screen
+screen.onkey(key='c', fun=clear_drawing)
+
 
 screen.exitonclick()
