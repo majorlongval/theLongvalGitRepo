7f50be0c3ec9695c716804928031a68fded9ada0
JordanLongval <j.longval@robotiq.com>
2020-12-07 22:32:49 -0500

    Created the seup function. not the clearest but its concise and does job

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
+screen_w = 500
+screen_h = 400
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(title='Make your bet', prompt="Which turtle will win the race?")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+
+def setup_game():
+    set_of_turtles = []
+    for color, i in zip(colors, range(0, len(colors))):
+        tmp_turtle = Turtle(shape='turtle')
+        tmp_turtle.color(color)
+        tmp_turtle.penup()
+        tmp_turtle.goto(x=-230, y=(-screen_h/(2*len(colors)) + (i-2)*screen_h/(2*len(colors))))
+        set_of_turtles.append(tmp_turtle)
+    return set_of_turtles
+
+
+setup_game()
+
+
+
+
+
+
 
 screen.exitonclick()
