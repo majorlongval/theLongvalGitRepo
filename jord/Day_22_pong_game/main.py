9e9230cb15afe61e5cab6ee0d9d961659e445fe4
jordan <jordan.longval@outlook.com>
2020-12-21 23:19:36 -0500

    WIP

---
+++ jord/Day_22_pong_game/main.py
@@ -0,0 +1,13 @@
+from turtle import Screen, Turtle
+
+# Defining the screen of the game
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+
+tim = Turtle()
+
+
+
+screen.exitonclick()
